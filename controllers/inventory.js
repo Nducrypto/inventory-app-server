379fa5ba9c4971c45278249632e778a40961c54d
nducrypto <agbonud9@gmail.com>
2023-03-02 22:02:44 +0100

    refactored cat

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -26,15 +26,57 @@ export const getTransaction = async (req, res) => {
 };
 
 export const createTransaction = async (req, res) => {
-  const transaction = req.body;
-
-  const newTransaction = new Inventory(transaction);
-
   try {
-    await newTransaction.save();
-    res.status(201).json(newTransaction);
+    const category = new RegExp(req.body.category, "i");
+
+    const item = await Inventory.findOne({
+      category: category,
+      creator: req.body.creator,
+    });
+    // console.log(item);
+    if (item && item.type === req.body.type) {
+      item.type = item.type;
+      item.category = item.category;
+      item.price = req.body.price;
+      item.quantity = Number(item.quantity) + Number(req.body.quantity);
+      item.quantitySold = item.quantitySold;
+      item.outgoingCost = item.outgoingCost;
+      item.totalCost = Number(item.totalCost) + Number(req.body.totalCost);
+
+      await item.save();
+
+      res.status(201).json(item);
+    } else if (item && item.type !== req.body.type) {
+      item.type = item.type;
+      item.category = item.category;
+      item.price = req.body.price;
+      item.quantity = Number(item.quantity) - Number(req.body.quantity);
+      item.quantitySold = Number(item.quantitySold) + Number(req.body.quantity);
+      item.outgoingCost =
+        Number(item.outgoingCost) + Number(req.body.totalCost);
+      item.totalCost = item.totalCost;
+
+      await item.save();
+
+      res.status(201).json(item);
+    } else {
+      const newTransaction = new Inventory({
+        type: req.body.type,
+        category: req.body.category,
+        quantity: req.body.quantity,
+        totalCost: req.body.totalCost,
+        outgoingCost: 0,
+        price: req.body.price,
+        quantitySold: 0,
+        creator: req.body.creator,
+        date: req.body.date,
+      });
+      await newTransaction.save();
+
+      res.status(201).json(newTransaction);
+    }
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    console.log(error);
   }
 };
 
@@ -74,7 +116,7 @@ export const inventoryStats = async (req, res, next) => {
     const income = await Inventory.find({ date: { $gte: lastMonth } });
     res.status(200).json(income);
   } catch (err) {
-    res.status(409).json({ message: error.message });
+    res.status(409).json({ message: err.message });
   }
 };
 
--- models/inventorySchema.js
+++ models/inventorySchema.js
@@ -5,8 +5,10 @@ const inventorySchema = mongoose.Schema(
     type: { type: String, required: true },
     category: { type: String, required: true },
     quantity: { type: Number, required: true },
-    amount: { type: Number, required: true },
+    totalCost: { type: Number, required: true },
+    outgoingCost: { type: Number, required: true },
     price: { type: Number, required: true },
+    quantitySold: { type: Number },
     creator: String,
     date: {
       type: Date,
